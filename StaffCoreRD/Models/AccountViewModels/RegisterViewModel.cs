5029e6fdbea37033420f3a30e7769fba2020f637
Luis Alejandro Bravo Bello <lb2024-1279@uce.edu.do>
2026-07-24 09:30:37 -0400

    feat:  Creacion de ViewModels de Login y Register

---
+++ StaffCoreRD/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StaffCoreRD.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo inválido")]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Confirma la contraseña")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
